0073ece79cd3f4f44cf38907713963a24430e4d2
star <87626214+anomyuser@users.noreply.github.com>
2025-12-30 05:05:38 +0900

    v1.1.1
    Just change gemini model name pro to flash (Simple error. Too minor to manage version)

---
+++ assets/save-BYLDBbIh.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-Bh2rpe7Q.js";/**
+ * @license lucide-react v0.559.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z",key:"nnexq3"}],["path",{d:"M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12",key:"mt58a7"}]],o=a("leaf",e);/**
+ * @license lucide-react v0.559.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z",key:"1c8476"}],["path",{d:"M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7",key:"1ydtos"}],["path",{d:"M7 3v4a1 1 0 0 0 1 1h7",key:"t51u73"}]],d=a("save",t);export{o as L,d as S};
